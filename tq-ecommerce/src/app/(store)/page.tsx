a7ecee76214099de144dc2770c751b399e6c141c
Agustin Blanchard <lagustinblanchard@gmail.com>
2026-05-26 15:44:54 -0300

    fix: force dynamic on homepage to fix Vercel build trace error

--- tq-ecommerce/src/app/(store)/page.tsx
+++ tq-ecommerce/src/app/(store)/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { ArrowRight, Truck, Shield, RotateCcw, MessageCircle } from "lucide-react";
